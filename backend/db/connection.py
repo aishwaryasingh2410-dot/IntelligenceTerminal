064c8e8be4b478bd3d73c6a98e9828d08a97648b
Aishwarya Singh <aishwaryasingh2410@gmail.com>
2026-04-18 19:09:09 +0530

    Security fix: Remove hardcoded DB password and update .gitignore

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,9 +1,33 @@
+import os
+
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://aishwaryasingh2410_db_user:OcS6Wltiz3jNcA1g@cluster0.sdicuyj.mongodb.net/cursor_database"
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("MONGO_DB_NAME", "cursor_database")
 
 client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
 
-db = client["cursor_database"]
+RAW_OPTIONS_COLLECTION = "raw_options_chain"
+LEGACY_OPTIONS_COLLECTION = "options_chain"
+NIFTY_TICKS_COLLECTION = "nifty_ticks"
+ANALYTICS_SNAPSHOTS_COLLECTION = "analytics_snapshots"
+ANOMALY_SIGNALS_COLLECTION = "anomaly_signals"
+PROCESSING_CHECKPOINTS_COLLECTION = "processing_checkpoints"
+LATEST_MARKET_SUMMARY_COLLECTION = "latest_market_summary"
 
-options_collection = db["options_chain"]
+raw_options_collection = db[RAW_OPTIONS_COLLECTION]
+options_collection = db[LEGACY_OPTIONS_COLLECTION]
+nifty_ticks_collection = db[NIFTY_TICKS_COLLECTION]
+analytics_snapshots_collection = db[ANALYTICS_SNAPSHOTS_COLLECTION]
+anomaly_signals_collection = db[ANOMALY_SIGNALS_COLLECTION]
+processing_checkpoints_collection = db[PROCESSING_CHECKPOINTS_COLLECTION]
+latest_market_summary_collection = db[LATEST_MARKET_SUMMARY_COLLECTION]
+
+
+def get_database():
+    return db
+
+
+def get_collection(name):
+    return db[name]
